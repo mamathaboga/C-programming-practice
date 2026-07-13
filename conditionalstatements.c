c1c412f271958ee340b80945fe7809e8963ad98d
mamathaboga <mamathaboga328@gmail.com>
2026-07-13 22:19:30 +0530

    Added conditional statements

---
+++ conditionalstatements.c
@@ -0,0 +1,30 @@
+//else if conditional statements
+/*#include<stdio.h>
+int main(){
+    int x=2;
+    if(x==1)
+    printf("x is 1\n");
+    else if(x==2)
+    printf("x is 2\n");
+    else if(x==3)
+    printf("x is 3");
+    else
+    printf("the number is other than 1,2 and 3");
+    return 0;
+}*/
+//switch conditional statements
+#include<stdio.h>
+int main(){
+    int x=2;
+    switch(x)
+    {
+        case 1:printf("x is 1\n");
+        break;
+        case 2:printf("x is 2\n");
+        break;
+        case 3:printf("x is 3\n");
+        break;
+        default:printf("the number is other than 1,2 and 3");
+    }
+    return 0;
+}
